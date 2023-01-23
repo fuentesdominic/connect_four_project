5dc2d19a2751e891e678a8eae8aecd78a964a186
Dominic Fuentes <militaryforce1@yahoo.com>
2023-01-23 14:03:41 -0600

    first js function and gameboard set up

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-const playerOne = 'Player One';
-const playerTwo = 'Player Two';
+const playerRed = 'red';
+const playerYellow = 'yellow';
+
+let currentPlayer = playerYellow;
+let rows = 6;
+let columns = 7;
+let gameBoard = gameBoard;
+
+function gamestart() {
+    gameBoard[];
+    for(let rows = 0; rows < rows; rows++) {
+        let rows[];
+        for(let columns = 0; columns < columns; columns++) {
+            let circles = document.createElement('div');
+            circles.id = rows.toString() + '.' + columns.toString();
+            circles.classList.add('circles');
+            circles.getElementsById('gameBoard').append(circles);
+    }
+}
