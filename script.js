3d9efaec3b6808bac3225662c7ba94ac5eb852d8
Dominic Fuentes <militaryforce1@yahoo.com>
2023-01-24 13:04:19 -0600

    finishing first function

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
 const playerRed = 'red';
 const playerYellow = 'yellow';
 
-let currentPlayer = playerYellow;
-let rows = 6;
-let columns = 7;
-let gameBoard = gameBoard;
+let currentPlayer = playerRed;
+const gameBoard = document.querySelector('.gameboard')
 
-windows.onload = function {
-    gameBoard();
-}
 
 function gamestart() {
-    gameBoard[];
-    for(let rows = 0; i < rows; i++) {
-        let rows[];
-        for(let columns = 0; i < columns; i++) {
-            rows.push();
-            let circles = document.createElement('div');
-            circles.id = rows.toString() + '.' + columns.toString();
-            circles.classList.add('circles');
-            circles.getElementsById('gameBoard').append(circles);
-        }
+    for(let i = 0; i < gameBoard.children.length; i++) {
+        gameBoard.children[i].addEventListener('click', () => {
+         if(currentPlayer === 'red') {
+              gameBoard.children[i].style.backgroundColor = 'red';
+                currentPlayer = playerYellow
+         } else if (currentPlayer === 'yellow') {
+            gameBoard.children[i].style.backgroundColor = 'yellow';
+                currentPlayer = playerRed
+       }
+
+       })
+    
     }
-}
-
-
-const winningNumbers = {
 
 }
+gamestart()
+
+const winningNumbers = [
+// horizontal 
+[0,1,2,3],
+[1,2,3,4],
+[2,3,4,5],
+[3,4,5,6],
+[7,8,9,10],
+[8,9,10,11],
+[9,10,11,12],
+[10,11,12,13],
+[14,15,16,17],
+[15,16,17,18],
+[16,17,18,19],
+[17,18,19,20],
+[21,22,23,24],
+[22,23,24,25],
+[23,24,25,26],
+[24,25,26,27],
+[28,29,30,31],
+[29,30,31,32],
+[30,31,32,33],
+[31,32,33,34],
+[35,36,37,38],
+[36,37,38,39],
+[37,38,39,40],
+[38,39,40,41],
+// vertical 
+[0,7,14,21],
+[7,14,21,28],
+[14,21,28,35]
+
+// diagonal
+]
